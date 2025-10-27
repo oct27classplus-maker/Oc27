619c61dbb1a2880aeca1cd8d7153c3696676e3ac
oct27classplus-maker <oct27classplus@gmail.com>
2025-10-27 21:11:10 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -11,12 +11,13 @@ OWNER = int(environ.get("OWNER", "7361052650"))
 CREDIT = environ.get("CREDIT", "Prunus🚬💋")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7361052650').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7361052650').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
